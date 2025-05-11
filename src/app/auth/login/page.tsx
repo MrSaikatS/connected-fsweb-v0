4dc9e8de6008893ae688ecdfdda7ea60d7a053bb
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-11 16:53:38 +0530

    user login route and form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,36 @@
+import LoginForm from "@/components/Forms/LoginForm";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
 const page = () => {
   return (
     <>
-      <div className=""></div>
+      <div className="grid h-[90dvh] place-items-center">
+        <Card className="w-[300px] sm:w-[400px]">
+          <CardHeader>
+            <CardTitle className="text-center text-2xl">User Login</CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <LoginForm />
+          </CardContent>
+
+          <CardFooter className="flex justify-center gap-2">
+            Don&apos;t have an account?
+            <Link
+              href="/auth/register"
+              className="hover:text-primary underline">
+              Register
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
     </>
   );
 };
--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -9,5 +9,5 @@ export const registerSchema = z.object({
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
-  rememberMe: z.boolean().default(false),
+  rememberMe: z.boolean(),
 });
