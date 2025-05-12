24737ad1e3818c3d6a9b64ee57d5d96a8eda1830
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-12 22:02:34 +0530

    better auth id and password modifications

---
+++ src/lib/argon2.ts
@@ -0,0 +1,23 @@
+import { hash, verify } from "@node-rs/argon2";
+import { serverEnv } from "./env/serverEnv";
+
+type VerifyPasswordParameter = {
+  hash: string;
+  password: string;
+};
+
+export const hashPasswordFunction = async (password: string) => {
+  const hashedPassword = await hash(password, {
+    secret: Buffer.from(serverEnv.BETTER_AUTH_SECRET),
+  });
+
+  return hashedPassword;
+};
+
+export const verifyPasswordFunction = async (data: VerifyPasswordParameter) => {
+  const { hash, password } = data;
+
+  const verifiedPassword = await verify(hash, password);
+
+  return verifiedPassword;
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
+import { hashPasswordFunction, verifyPasswordFunction } from "./argon2";
 import prisma from "./db/prisma";
 import { serverEnv } from "./env/serverEnv";
 
@@ -14,8 +15,15 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: false,
     requireEmailVerification: false,
+    password: {
+      hash: hashPasswordFunction,
+      verify: verifyPasswordFunction,
+    },
   },
   advanced: {
     cookiePrefix: "connected",
+    database: {
+      generateId: false,
+    },
   },
 });
