3b9d33f1941db879f6924930d561a909393a4d7c
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-18 22:22:35 +0530

    Update layout.tsx

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -1,7 +1,20 @@
 import Header from "@/components/Header/Header";
+import { auth } from "@/lib/auth";
 import { LayoutProps } from "@/lib/types";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const ApplicationLayout = ({ children }: LayoutProps) => {
+const ApplicationLayout = async ({ children }: LayoutProps) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
+  console.log(session);
+
   return (
     <>
       <Header appName="NST App" />
